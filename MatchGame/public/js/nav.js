9870947fe04ba14a4e63525275dfff87a4efc26f
Miguel Pacheco <minipachru@gmail.com>
2021-10-28 21:13:06 -0300

    returned to previous state

---
+++ MatchGame/public/js/nav.js
@@ -0,0 +1,27 @@
+const home = document.querySelector('#home');
+const feature = document.querySelector('#features');
+const about = document.querySelector('#about');
+const team = document.querySelector('#team');
+const contact = document.querySelector('#contact');
+
+
+home.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
+feature.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
+about.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
+team.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
+contact.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
